84599af2d679daeb3b83bda81527444245ecd02f
kangajo <abduljohar99@gmail.com>
2026-02-16 18:57:08 +0700

    update

---
+++ .vitepress/config.js
@@ -0,0 +1,12 @@
+export default {
+  title: "Kamus Coding kangajo",
+  themeConfig: {
+    search: {
+      provider: "local",
+    },
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Panduan", link: "/README" },
+    ],
+  },
+};
